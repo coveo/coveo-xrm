bdbf6e4b12c52af2291e6aa200d65cd076afd76b
Sebastien Belzile <sbelzile@coveo.com>
2018-06-04 12:34:45 -0400

    update odata dependency

--- src/data/entity/EntityWrapper.ts
+++ src/data/entity/EntityWrapper.ts
@@ -1,12 +1,16 @@
 export interface IEntity {
     getId: () => string;
+    save(saveMode?: Xrm.EntitySaveMode)
 }
 
 export class EntityWrapper implements IEntity {
-    constructor(private entity: Xrm.Entity) {
-    }
+    constructor(private entity: Xrm.Entity) {}
 
     getId() {
         return this.entity.getId();
+    }
+
+    save(saveMode?: Xrm.EntitySaveMode) {
+        this.entity.save(saveMode);
     }
 }
